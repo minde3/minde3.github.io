796c9c68fedb99f75cfedbf33726832719f4aede
박세환 <127464776+minde3@users.noreply.github.com>
2023-03-24 11:34:12 +0900

    Delete C1형-기본구조 directory

--- C1형-기본구조/script/script.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-
-    $(".mian>li").mousover(function(){
-        $(this).find(".sub").stop().slideDown()
-    }).mouseout(function(){
-        $(this).find(".sub").stop().slideUp()
-    })
-})
