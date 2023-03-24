07c158f8b0ec29e23839c762f9b93e51ee956093
박세환 <127464776+minde3@users.noreply.github.com>
2023-03-24 11:34:02 +0900

    Delete C3형-기본구조 directory

--- C3형-기본구조/script/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-
-    $(".main>li").mouseover(function(){
-        $(this).find(".sub").stop().slideDown()
-    }).mouseout(function(){
-        $(this).find(".sub").stop().slideUp()
-    })
-    
-})
